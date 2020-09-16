9f82f26b6ef67b4abcd54ebcb8b4d3a0ecf3a11e
Brian Henry <BrianHenryIE@gmail.com>
2020-09-15 23:56:51 -0700

    Use WP cache for Settings

--- src/api/class-settings.php
+++ src/api/class-settings.php
@@ -23,20 +23,6 @@ class Settings implements Settings_Interface {
 	const SUBJECT_FILTER_REGEX_DICTIONARY = 'bh_wp_autologin_urls_subject_filter_regex_dictionary';
 
 	/**
-	 * The expiry time as set on the settings page.
-	 *
-	 * @var int The chosen expiry time in seconds.
-	 */
-	private $expiry_time;
-
-	/**
-	 * Indicates if autologin codes should be added to emails to admins.
-	 *
-	 * @var bool Should add autologin codes for admins.
-	 */
-	private $autologin_for_admins_is_enabled = false;
-
-	/**
 	 * A dictionary of regex:notes, where the regex is applied to the email subject to
 	 * disable adding the autologin code and the notes are for the admin UI to remind the
 	 * user what the regex means.
@@ -52,21 +38,13 @@ class Settings implements Settings_Interface {
 	 */
 	public function __construct() {
 
-		$expiry_time       = get_option( self::EXPIRY_TIME_IN_SECONDS, 604800 );
-		$this->expiry_time = is_int( intval( $expiry_time ) ) && $expiry_time > 0 ? intval( $expiry_time ) : 604800;
-
-		$autologin_for_admins_is_enabled       = get_option( self::ADMIN_ENABLED, 'admin_is_not_enabled' );
-		$this->autologin_for_admins_is_enabled = 'admin_is_enabled' === $autologin_for_admins_is_enabled;
-
-		$default_subject_regexes = array(
+		$this->disallowed_subjects_regex_dictionary = array(
 			'/^.*Login Details$/'                   => '[Example Site] Login Details',
 			'/^.*Your new password$/'               => 'Example Site Your new password',
 			'/^Password Reset Request.*$/'          => 'Password Reset Request for Example Site',
 			'/^Please complete your registration$/' => 'Please complete your registration',
 		);
 
-		$disallowed_subject_regex_dictionary        = get_option( self::SUBJECT_FILTER_REGEX_DICTIONARY, $default_subject_regexes );
-		$this->disallowed_subjects_regex_dictionary = is_array( $disallowed_subject_regex_dictionary ) ? $disallowed_subject_regex_dictionary : $default_subject_regexes;
 	}
 
 	/**
@@ -74,7 +52,7 @@ class Settings implements Settings_Interface {
 	 *
 	 * @return int The expiry time in seconds, as set on the settings page.
 	 */
-	public function get_expiry_age() {
+	public function get_expiry_age(): int {
 		$expiry_time       = get_option( self::EXPIRY_TIME_IN_SECONDS, 604800 );
 		return intval( $expiry_time ) > 0 ? intval( $expiry_time ) : 604800;
 	}
@@ -84,8 +62,10 @@ class Settings implements Settings_Interface {
 	 *
 	 * @return bool Should the autologin code be added to urls in emails sent to admins?
 	 */
-		return $this->autologin_for_admins_is_enabled;
 	public function get_add_autologin_for_admins_is_enabled(): bool {
+
+		$autologin_for_admins_is_enabled = get_option( self::ADMIN_ENABLED, 'admin_is_not_enabled' );
+		return  'admin_is_enabled' === $autologin_for_admins_is_enabled;
 	}
 
 	/**
@@ -93,8 +73,8 @@ class Settings implements Settings_Interface {
 	 *
 	 * @return string[]
 	 */
-		return array_keys( $this->disallowed_subjects_regex_dictionary );
 	public function get_disallowed_subjects_regex_array(): array {
+		return array_keys( $this->get_disallowed_subjects_regex_dictionary() );
 	}
 
 	/**
@@ -102,8 +82,14 @@ class Settings implements Settings_Interface {
 	 *
 	 * @return string[]
 	 */
-		return $this->disallowed_subjects_regex_dictionary;
 	public function get_disallowed_subjects_regex_dictionary(): array {
+
+		$disallowed_subjects_regex_dictionary = get_option( self::SUBJECT_FILTER_REGEX_DICTIONARY, $this->disallowed_subjects_regex_dictionary );
+
+		$disallowed_subjects_regex_dictionary = is_array( $disallowed_subjects_regex_dictionary ) ? $disallowed_subjects_regex_dictionary : $this->disallowed_subjects_regex_dictionary;
+
+		return $disallowed_subjects_regex_dictionary;
+
 	}
 
 }
