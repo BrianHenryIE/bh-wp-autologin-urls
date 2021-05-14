459b5a7f5a2969c6af19656c298189c6c83a47f8
Brian Henry <BrianHenryIE@gmail.com>
2021-05-14 15:42:01 -0700

    Mozart -> strauss

--- src/autoload.php
+++ src/autoload.php
@@ -15,27 +15,7 @@ namespace BH_WP_Autologin_URLs;
 
 use BrianHenryIE\WP_Autologin_URLs\Pablo_Pacheco\WP_Namespace_Autoloader\WP_Namespace_Autoloader;
 
-
-// The plugin-scoped namespace for composer required libraries, as specified in composer.json Mozart config.
-$dep_namespace = 'BH_WP_Autologin_URLs';
-// The Mozart config `dep_directory` adjusted for relative path.
-$dep_directory = '/vendor/';
-
-spl_autoload_register(
-	function ( $namespaced_class_name ) use ( $dep_namespace, $dep_directory ) {
-
-		$autoload_directory = __DIR__ . $dep_directory . '/';
-
-		// The class name with its true namespace.
-		$bare_namespaced_class_name = preg_replace( "#$dep_namespace\\\*#", '', $namespaced_class_name );
-
-		$file_path = $autoload_directory . str_replace( '\\', '/', $bare_namespaced_class_name ) . '.php';
-
-		if ( file_exists( $file_path ) ) {
-			require_once $file_path;
-		}
-	}
-);
+require_once __DIR__ . '/strauss/autoload.php';
 
 $wpcs_autoloader = new WP_Namespace_Autoloader();
 $wpcs_autoloader->init();
