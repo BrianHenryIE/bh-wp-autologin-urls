ca436fb0393b0bc0dd6f8a0cd9c89b10135d1de5
Brian Henry <BrianHenryIE@gmail.com>
2022-03-10 12:52:21 -0800

    Remove "logs" tab from settings page.

--- src/Admin/Partials/admin-display.php
+++ src/Admin/Partials/admin-display.php
@@ -4,22 +4,16 @@
  *
  * This file is used to markup the admin-facing aspects of the plugin.
  *
+ * @see \BrianHenryIE\WP_Autologin_URLs\Admin\Settings_Page::display_plugin_admin_page()
+ *
+ * @var \Psr\Log\LoggerInterface $logger The logger made available from the calling function.
+ * @var string $example_url A URL (non-working) which looks like how the real ones will appear.
+ *
  * @link       https://BrianHenry.ie
  * @since      1.0.0
  *
- * @package    bh-wp-autologin-urls
- * @subpackage bh-wp-autologin-urls/admin/partials
+ * @package    brianhenryie/bh-wp-autologin-urls
  */
-
-use BrianHenryIE\WP_Autologin_URLs\WP_Logger\Admin\Logs_Table;
-
-/** @var \Psr\Log\LoggerInterface $logger */
-/** @var string $example_url */
-
-// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-$active_tab = isset( $_GET['tab'] ) ? filter_input( INPUT_GET, 'tab', FILTER_SANITIZE_STRING ) : 'settings';
-$active_tab = in_array( $active_tab, array( 'settings', 'logs' ), true ) ? $active_tab : 'settings';
-
 
 ?>
 
@@ -35,13 +29,6 @@ $active_tab = in_array( $active_tab, array( 'settings', 'logs' ), true ) ? $acti
 
 	<p>With this plugin enabled, when emails are sent from WordPress (and its plugins) to users, a login code is added to each URL in the emails, which automatically logs users into the site when they visit. Modified URLs take the form <span class="url"><?php echo esc_url( $example_url ); ?></span>, and will often be transparent to users, due to HTML formatted (rich text) emails.</p>
 
-	<h2 class="nav-tab-wrapper">
-		<a href="options-general.php?page=bh-wp-autologin-urls&tab=settings" class="nav-tab <?php echo $active_tab === 'settings' ? 'nav-tab-active' : ''; ?>">Settings</a>
-		<a href="options-general.php?page=bh-wp-autologin-urls&tab=logs" class="nav-tab <?php echo $active_tab === 'logs' ? 'nav-tab-active' : ''; ?>">Logs</a>
-	</h2>
-
-<?php if ( 'settings' === $active_tab ) { ?>
-
 	<p>To exclude emails with particular subjects from having autologin codes added, use regular expressions to match the subjects. e.g. password reset email does not need an autologin code added to its URLs.</p>
 
 	<form method="POST" action="options.php">
@@ -52,18 +39,6 @@ $active_tab = in_array( $active_tab, array( 'settings', 'logs' ), true ) ? $acti
 		?>
 	</form>
 
-	<?php
-} elseif ( 'logs' === $active_tab ) {
-
-	// TODO: Logger should have an easy way to display the table.
-	// $logs_table = new Logs_Table( $api, $settings, $logger );
-	//
-	// $logs_table->prepare_items();
-	// $logs_table->display();
-
-}
-?>
-
-	<p><a href="https://wordpress.org/support/plugin/bh-wp-autologin-urls">Support on WordPress.org</a> &#x2022; <a href="https://github.com/BrianHenryIE/BH-WP-Autologin-URLs">Code on GitHub</a> &#x2022; <a href="https://BrianHenry.ie">Plugin by BrianHenryIE</a></p>
+	<p><a href="https://wordpress.org/support/plugin/bh-wp-autologin-urls">Support on WordPress.org</a> &#x2022; <a href="https://github.com/BrianHenryIE/bh-wp-autologin-urls">Code on GitHub</a> &#x2022; <a href="https://BrianHenry.ie">Plugin by BrianHenryIE</a></p>
 
 </div>
