7107dab5703193f0e6354fea554c11c9efabd210
Brian Henry <BrianHenryIE@gmail.com>
2022-08-04 13:02:36 -0700

    Fix: fatal error when `plugin_action_links_{}` filter called with null parameters
    By Jetpack

--- src/Admin/class-plugins-page.php
+++ src/Admin/class-plugins-page.php
@@ -38,13 +38,13 @@ class Plugins_Page {
 	 *
 	 * @hooked plugin_action_links_{basename}
 	 *
-	 * @param array<int|string, string>  $links_array The existing plugin links (usually "Deactivate"). May or may not be indexed with a string.
-	 * @param string                     $plugin_file The plugin basename.
-	 * @param array<string, string|bool> $plugin_data The parsed plugin header data.
-	 * @param string                     $context 'all'|'active'|'inactive'...
+	 * @param array<int|string, string>   $links_array The existing plugin links (usually "Deactivate"). May or may not be indexed with a string.
+	 * @param ?string                     $plugin_file The plugin basename.
+	 * @param ?array<string, string|bool> $plugin_data The parsed plugin header data.
+	 * @param ?string                     $context 'all'|'active'|'inactive'...
 	 * @return array<int|string, string> The links to display below the plugin name on plugins.php.
 	 */
-	public function action_links( array $links_array, string $plugin_file, array $plugin_data, string $context ): array {
+	public function action_links( array $links_array, ?string $plugin_file, ?array $plugin_data, ?string $context ): array {
 
 		$settings_url = admin_url( '/options-general.php?page=' . $this->settings->get_plugin_slug() );
 
